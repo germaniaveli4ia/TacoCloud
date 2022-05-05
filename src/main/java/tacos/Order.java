50a16e9ddfda9aefd6ad2d9cbbfa53ff5857d7d8
alkapolly <germaniaveli4ia@gmail.com>
2022-05-05 10:14:38 +0300

    added db

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -2,15 +2,53 @@ package tacos;
 
 import lombok.Data;
 
-@Data
-public class Order {
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+@Data
+@Entity
+@Table(name="Taco_Order")
+public class Order implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Date placeddAt;
+
+    @NotBlank(message = "This field is required")
     private String name;
+    @NotBlank(message = "This field is required")
     private String street;
+    @NotBlank(message = "This field is required")
     private String city;
+    @NotBlank(message = "This field is required")
     private String state;
+    @NotBlank(message = "This field is required")
     private String zip;
+    @NotBlank(message = "This field is required")
     private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
     private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        this.tacos.add(design);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placeddAt = new Date();
+    }
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,14 +1,39 @@
 package tacos;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt;
+
+    @NonNull
+    @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
+    @Size(min=1, message = "Choose at least 1 ingredient")
     private List<String> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+
+    public Taco() {
+
+    }
 }
